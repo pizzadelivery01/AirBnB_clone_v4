afbeeda12d4e9ba26ac4b24f21c60cd47c404817
jenntang1 <1039@holbertonschool.com>
2020-05-18 19:06:40 +0000

    Update append

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -40,9 +40,7 @@ window.onload = function () {
     success: function (data) {
       for (let i = 0; i < data.length; i++) {
         const place = data[i];
-        $('places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div>');
-        $('places').append('<div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div>');
-        $('places').append('<div class="description">' + place.description + '</div></article>');
+        $('places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
       }
     }
   });
